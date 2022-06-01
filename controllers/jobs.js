df3fbe99343550476eba6be550fd7a6d6da1d1f1
aravind-812 <aravindkamarsu32@gmail.com>
2022-06-01 23:54:57 +0530

    basic structure

---
+++ controllers/jobs.js
@@ -0,0 +1,16 @@
+const getAllJobs = async (req, res) => {
+  res.send("User Registered");
+};
+
+const createJob = async (req, res) => {
+  res.send("login user");
+};
+
+const createJob = async (req, res) => {
+  res.send("login user");
+};
+
+module.exports = {
+  getAllJobs,
+  createJob,
+};
